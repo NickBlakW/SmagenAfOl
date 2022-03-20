9de990547e5b81937961a7ca86f7b05b4a40c718
Nicolai Blak Walther <niwal19@student.sdu.dk>
2022-03-20 18:06:09 +0100

    Cleanup
    Small changes to bat - now opens website
    Css correction for Modal
    Small change in footer

--- resources/views/standard/footer.blade.php
+++ resources/views/standard/footer.blade.php
@@ -1,16 +1,16 @@
 <div class="info">
     <div class="info-left">
-		<p class="info-margin info-text"><b>Heinrich Loff, Beer Somalier</b></p>
         <p class="info-margin info-text">Hærvejen 73, 6230 Rødekro</p>
         <p class="info-margin info-text">Mobil: 5049 9012</p>
         <p class="info-margin info-text">smagenafoel@gmail.com</p>
         <p class="info-margin info-text">CVR.nr.: 38 72 08 48</p>
     </div>
     <div class="info-center">
+        <p class="info-margin info-text"><b>Heinrich Loff, Beer Somalier</b></p>
         <!-- Åbningstider og link til facebook -->
         <p class="info-margin info-text">
             <a class="footer-link" href="https://www.facebook.com/Smagen-af-Øl-1685648551709238/" target="_blank">
-                Facebook
+                <em>Facebook</em>
             </a>
         </p>
     </div>
